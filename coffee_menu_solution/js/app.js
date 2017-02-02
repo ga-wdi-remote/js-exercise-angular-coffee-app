db5058c8cffc421977d5131ab4427d2ea5302625
Christine Yi <christineyi@Christines-MacBook-Pro.local>
2017-02-02 11:43:12 -0500

    pushing coffee app stuffs

---
+++ coffee_menu_solution/js/app.js
@@ -0,0 +1,61 @@
+angular.module("myApp", [])
+.controller("mainController", mainController);
+
+function mainController(){
+  var main = this;
+  main.menu = menuList;
+  main.show = function(item){
+    main.coffee = item;
+  };
+}
+
+var menuList = [
+  {
+    name: "Plain Ole Coffee",
+    price: 2,
+    img: "images/coffee.jpg",
+    size: "small"
+  },
+  {
+    name: "Espresso Shot",
+    price: 2.25,
+    img: "images/espresso.jpg",
+    size: "small"
+  },
+  {
+    name: "Cafe Au Lait",
+    price: 3,
+    img: "images/cafe-au-lait.jpg",
+    size: "small"
+  },
+  {
+    name: "Cappuccino",
+    price: 5,
+    size: "medium",
+    img: "images/cappuccino.jpg"
+  },
+  {
+    name: "Latte",
+    price: 5,
+    size: "medium",
+    img: "images/latte.jpg"
+  },
+  {
+    name: "Hot Chocolate",
+    price: 4,
+    size: "medium",
+    img: "images/hotchocolate.jpg"
+  },
+  {
+    name: "Mocha",
+    price: 6,
+    size: "large",
+    img: "images/mocha.jpg"
+  },
+  {
+    name: "Jumbo Coffee",
+    price: 40,
+    size: "large",
+    img: "images/extreme.jpg"
+  }
+];
